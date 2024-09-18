b85c402a814010fb4ebed94502b1ed9587684b31
natsuriusz <cezary.serozynski@gmail.com>
2024-09-18 20:24:50 +0200

    CreatingChildClasses

---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+// Guided project nr 2
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Aura/Public/Character/EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Guided project nr 2
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AEnemyCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
