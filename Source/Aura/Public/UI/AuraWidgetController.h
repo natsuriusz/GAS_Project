61203f30b18e3167cff06fd730b4cbed81e7a7a8
natsuriusz <cezary.serozynski@gmail.com>
2024-11-01 18:35:51 +0100

    AuraWidgetController

---
+++ Source/Aura/Public/UI/AuraWidgetController.h
@@ -0,0 +1,17 @@
+// Guided project nr 2
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "AuraWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraWidgetController : public UObject
+{
+	GENERATED_BODY()
+	
+};
