993a211506e0cd878de51d30a216e53f635cddbf
natsuriusz <cezary.serozynski@gmail.com>
2024-11-01 18:33:54 +0100

    Aura User Widget

---
+++ Source/Aura/Public/UI/Widgets/AuraUserWidget.h
@@ -0,0 +1,17 @@
+// Guided project nr 2
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
