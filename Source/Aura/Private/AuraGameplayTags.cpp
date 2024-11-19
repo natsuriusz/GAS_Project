aa9434517982c5efa3b5a939d23a833b14dd30ef
natsuriusz <cezary.serozynski@gmail.com>
2024-11-19 17:30:52 +0100

    AuraGameplayTags

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,12 @@
+// Guided project nr 2
+
+
+#include "AuraGameplayTags.h"
+
+AuraGameplayTags::AuraGameplayTags()
+{
+}
+
+AuraGameplayTags::~AuraGameplayTags()
+{
+}
---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,15 @@
+// Guided project nr 2
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class AURA_API AuraGameplayTags
+{
+public:
+	AuraGameplayTags();
+	~AuraGameplayTags();
+};
