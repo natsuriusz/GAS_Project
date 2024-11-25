830ea93268172883222babde7f89c1ef29ace51e
natsuriusz <cezary.serozynski@gmail.com>
2024-11-25 21:44:28 +0100

    Replication of Target Data

--- Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "Abilities/Tasks/AbilityTask.h"
 #include "TargetDataUnderMouse.generated.h"
-
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FGameplayAbilityTargetDataHandle&, DataHandle);
 /**
  * 
  */
@@ -17,5 +17,12 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "Ability||Tasks", meta = (DisplayName = "TargetDataUnderMouse", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = true))
 	static UTargetDataUnderMouse* CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility);
-	
+
+	UPROPERTY(BlueprintAssignable)
+	FMouseTargetDataSignature ValidData;
+
+private:
+	virtual void Activate() override;
+	void SendMouseData();
+	void OnTargetDataReplicationCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag);
 };
