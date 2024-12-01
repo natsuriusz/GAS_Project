e132b290cfe4d17328ec5b75b72b7df8e9de784a
natsuriusz <cezary.serozynski@gmail.com>
2024-12-01 12:02:45 +0100

    Exec Calc

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,5 @@
+// Guided project nr 2
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
---
+++ Source/Aura/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
@@ -0,0 +1,17 @@
+// Guided project nr 2
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "ExecCalc_Damage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UExecCalc_Damage : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+	
+};
